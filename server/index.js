db711aed69adbb8f8f0af0e8368984bcbb1d7b53
Arran White <arrwhidev@gmail.com>
2016-09-01 23:16:32 +0100

    lol

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ const server = app.listen(port);
 
 server.on('listening', () => {
     console.log(`Feathers application started on ${app.get('host')}:${port}`);
+    console.log('You have to call me Dragon, you have to call me Nighthawk!');
     if (process.send) {
         process.send('online');
     }
-    
+
 });
