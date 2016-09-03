e1bb911c2c0ced4424b7ab08ec8ac0bf2096571d
arran <arrwhidev@gmail.com>
2016-09-03 14:25:01 +0100

    Renamed file and added id to title

--- client/components/SprintList/SprintListItem.js
+++ client/components/SprintList/SprintListItem.js
@@ -6,20 +6,20 @@ import cx from 'classnames';
 import { Card, CardTitle, CardContent } from '../Card';
 
 export default
-class SprintList extends React.Component {
+class SprintListItem extends React.Component {
     static propTypes = {
         className: PropTypes.string
     };
 
     render() {
-        const { title, description, className } = this.props;
+        const { id, title, description, className } = this.props;
         const classnames = cx({
             [className]: className
         });
 
         return (
             <Card className={classnames}>
-                <CardTitle title={title} />
+                <CardTitle title={`${id} - ${title}`} />
                 <CardContent>
                     <p>{description}</p>
                 </CardContent>
